cd73e4f6872235f2641c9faf301fac0b53474605
Melike <168220231+Mellikke@users.noreply.github.com>
2024-06-26 20:31:49 +0300

    Add files via upload

---
+++ Proje2main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proje2main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+
+/**
+ *
+ * @author yildi
+ */
+public class Proje2main {
+     
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+ 
+         
+    }
+    
+}
